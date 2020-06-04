514c801dfa6d23e00d12cc1cf8abe0c8543e16d9
MurrTuck <murrtuck@gmail.com>
2020-06-04 15:27:42 -0600

    Full Stack Review Complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 // import logo from './logo.svg';
+import Header from './components/Header'
+import AuthHeader from './components/AuthHeader'
 import routes from './routes'
 import { connect } from 'react-redux'
 import './App.css';
@@ -8,6 +10,7 @@ function App(props) {
   // console.log('Look at me!', props)
   return (
     <div className="App">
+      {props.isLoggedIn ? <Header /> : <AuthHeader />}
       {routes}
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = props => {
+    return (
+        <nav>
+            <h1>Welcome to Twitchumrechan</h1>
+            <div>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/profile">Profile</Link>
+            </div>
+        </nav>
+    )
+}
+export default Header
